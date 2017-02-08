ffaf5a1afa09cadadab52953e2821cb4c0e39f9c
Georgia Stuart <georgia.stuart@gmail.com>
2017-02-07 18:22:56 -0600

    Added siteurls to urls

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://utdweteachcs.org'
+SITEURL = 'http://georgiastuart.com/utdweteachcs_pelican'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
@@ -18,7 +18,7 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
-STATIC_PATHS = ['css', 'js', ]
+STATIC_PATHS = ['css', 'js', 'images']
 # Following items are often useful when publishing
 
 #DISQUS_SITENAME = ""
